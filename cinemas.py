24f0cc0c1d6b545ac2d7eaf464548b236ddc840d
mkoryakov <mnkoryakov@yandex.ru>
2017-01-22 18:10:36 +0300

    Убран изменяемый аргумент по умолчанию в функции fetch_html_page, изменены алгоритмы парсинга афиши и кинопоиска, удалены магические числа, переименованы плохо названные переменные.

--- cinemas.py
+++ cinemas.py
@@ -1,12 +1,14 @@
 import requests
 import time
 import re
-from collections import Counter
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 
 
-def fetch_html_page(url, payload={}):
+TIME_DELAY_SECONDS = 20
+
+
+def fetch_html_page(url, payload=None):
     return requests.get(url, payload).text
 
 
@@ -14,36 +16,34 @@ def parse_afisha_page(raw_html):
     html_parser = BeautifulSoup(raw_html, 'html.parser')
     movie_template = re.compile('www.afisha.ru/movie/\d+/')
     cinema_template = re.compile('www.afisha.ru/[a-z]+/cinema/\d+/')
-    movies = {}
-    for link in html_parser.find_all('a'):
-        href = link.get('href')
-        if movie_template.search(href):
-            movie_title = link.string
-            continue
-        if cinema_template.search(href):
-            if movie_title not in movies:
-                movies[movie_title] = Counter()
-            movies[movie_title]['count_cinemas'] += 1
+    movies = []
+    for link in html_parser.find_all('table'):
+        count_cinemas = len(link.find_all(href=cinema_template))
+        if count_cinemas:
+            movie_title = link.find_parent().find(href=movie_template).string
+            movies.append({})
+            movies[-1]['movie_title'] = movie_title
+            movies[-1]['count_cinemas'] = count_cinemas
     return movies
 
 
 def parse_kinopoisk_page(raw_html):
     html_parser = BeautifulSoup(raw_html, 'html.parser')
     votes_template = re.compile('/film/\d+/votes')
-    for link in html_parser.find_all('a'):
-        href = link.get('href')
-        if href and votes_template.search(href):
-            rating_ball = float(link.contents[1].string)
-            rating_count = link.contents[3].string
-            return rating_ball, rating_count
-    return 0.0, 0
+    rating_ball = 0.0
+    rating_count = 0
+    for link in html_parser.find_all(href=votes_template):
+        rating_ball = float(link.contents[1].string)
+        rating_count = link.contents[3].string
+        break
+    return rating_ball, rating_count
 
 
 def delete_art_house_movies(movies, min_count_cinemas):
-    not_art_house_movies = {}
-    for movie_title in movies:
-        if movies[movie_title]['count_cinemas'] >= min_count_cinemas:
-            not_art_house_movies[movie_title] = movies[movie_title]
+    not_art_house_movies = []
+    for movie in movies:
+        if movie['count_cinemas'] >= min_count_cinemas:
+            not_art_house_movies.append(movie)
     return not_art_house_movies
 
 
@@ -57,24 +57,24 @@ def fetch_movies_title_from_afisha():
 def find_movies_rating(movies):
     url = 'https://www.kinopoisk.ru/index.php'
     payload = {'first': 'yes'}
-    for movie_title in movies:
-        payload['kp_query'] = movie_title
+    for movie in movies:
+        payload['kp_query'] = movie['movie_title']
         raw_html = fetch_html_page(url, payload)
         rating_ball, rating_count = parse_kinopoisk_page(raw_html)
-        movies[movie_title]['rating_ball'] = rating_ball
-        movies[movie_title]['rating_count'] = rating_count
-        time.sleep(20)
+        movie['rating_ball'] = rating_ball
+        movie['rating_count'] = rating_count
+        time.sleep(TIME_DELAY_SECONDS)
 
 
-def sorted_movies_by_rating(movies):
-    sorted(movies,
-           key=lambda movie_title: (movies[movie_title]['rating_ball']))
+def sort_movies_by_rating(movies):
+    sorted(movies, key=lambda movie: movie['rating_ball'])
 
 
 def output_movies_to_console(movies, count_popular_movies):
-    for movie_title in list(movies.keys())[:count_popular_movies]:
-        count_cinemas = movies[movie_title]['count_cinemas']
-        rating_ball = movies[movie_title]['rating_ball']
+    for movie in movies[:count_popular_movies]:
+        movie_title = movie['movie_title']
+        count_cinemas = movie['count_cinemas']
+        rating_ball = movie['rating_ball']
         output_string = 'Фильм "%s" имеет рейтинг %.3f, его показывают в %d кинотеатрах'
         print(output_string % (movie_title, rating_ball, count_cinemas))
 
@@ -93,5 +93,5 @@ if __name__ == '__main__':
     if count_cinemas:
         movies = delete_art_house_movies(movies, count_cinemas)
     find_movies_rating(movies)
-    sorted_movies_by_rating(movies)
+    sort_movies_by_rating(movies)
     output_movies_to_console(movies, count_popular_movies)
